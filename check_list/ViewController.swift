7251a35137099f516a4e607a98cb62fba919c1fd
Eliardo <eliardosilva81@gmail.com>
2024-09-03 16:12:25 -0300

    Initial Commit

---
+++ check_list/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  check_list
+//
+//  Created by Eliardo Venancio on 03/09/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
